b02e9a7da313eab1003696b8911c9f35dd84d986
hfmsolucoes <hfm.solucoes@hotmail.com>
2019-06-28 23:10:45 -0300

    Aula #06_02 Validação de Appointment

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import UserController from './app/controllers/UserController';
 import ProvidersController from './app/controllers/ProvidersController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -16,6 +17,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProvidersController.index);
+routes.post('/appointment', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
