00905d7f827701de5cf015ff59073c0de75ccde2
hfmsolucoes <hfm.solucoes@hotmail.com>
2019-06-29 13:43:00 -0300

    Aula #13_02 Marcando notificação como lida

--- src/routes.js
+++ src/routes.js
@@ -23,6 +23,7 @@ routes.get('/appointment', AppointmentController.index);
 routes.post('/appointment', AppointmentController.store);
 routes.get('/schedule', ScheduleController.index);
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
