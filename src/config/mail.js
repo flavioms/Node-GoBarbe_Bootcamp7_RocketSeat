80d726704b4216639601d611f0ef6608c8e5c506
hfmsolucoes <hfm.solucoes@hotmail.com>
2019-06-29 14:17:08 -0300

    Aula #15_02 Envio de e-mail

---
+++ src/config/mail.js
@@ -0,0 +1,11 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '50bced451c7bda',
+    pass: '10f3a80b5c9f3e',
+  },
+  default: {
+    from: 'Equipe GObarber <noreply@gobarer.com>',
+  },
+};
